3152d3d6efb1aca54a0807ec0619349dd645da7b
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-08-02 19:36:20 +0200

    Restore ROBOTSTXT_OBEY rule, and format USER_AGENT value

--- webscraper/settings.py
+++ webscraper/settings.py
@@ -27,10 +27,12 @@ IMAGES_STORE = 'data/images/'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = 'webscraper (+http://www.yourdomain.com)'
-USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) " \
+             "AppleWebKit/537.36 (KHTML, like Gecko) " \
+             "Chrome/67.0.3396.99 Safari/537.36"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
